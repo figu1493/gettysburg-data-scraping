a6fffb86f978dd2628bb111c84827418bd0c4810
John Figueiredo <john@johnfigueiredo.com>
2013-06-13 19:03:31 -0700

    Use ActionMailer to send verification emails with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
   # attr_accessible :title, :body
+
+
+# Filter
+#-------------------------------------------------------------------------------------------------------------
+#
+
+#   after_save :send_email
+
+
+# # Methods
+# #-------------------------------------------------------------------------------------------------------------
+# #
+
+#   def send_email
+#   	UserMailer.welcome_email(@user).deliver
+#   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,13 @@ FourScore::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'john@johnfigueiredo.com',
+  :password             => 'jfig3333',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
 end
